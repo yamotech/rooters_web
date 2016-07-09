6de001d871e42370ffd19f598d8a95430c527e62
yamotech <nothings.2c9@gmail.com>
2016-07-09 09:55:20 +0900

    [add] seedデータ

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Sport.create(
+  sport_category_id: 1,
+  sport_name_ja: "開・閉会式",
+  sport_name_en: "Opening and Closing",
+  sport_type: "セレモニー",
+  sport_description: "開会式が行われます。",
+  stadium_id: 1,
+  sport_date: "2016-10-01",
+  disabled: "0"
+  )
