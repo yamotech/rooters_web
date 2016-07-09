ad18d63781aedd6a3558f0fd4431be8fc843b814
yamotech <nothings.2c9@gmail.com>
2016-07-09 11:58:01 +0900

    [add] ログイン機能実装

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160709003737) do
+ActiveRecord::Schema.define(version: 20160709023721) do
 
   create_table "sports", force: :cascade do |t|
     t.integer  "sport_category_id"
@@ -25,5 +25,34 @@ ActiveRecord::Schema.define(version: 20160709003737) do
     t.datetime "created_at",        null: false
     t.datetime "updated_at",        null: false
   end
+
+  create_table "stadia", force: :cascade do |t|
+    t.string   "stadium_name"
+    t.string   "address"
+    t.float    "lat"
+    t.float    "lon"
+    t.string   "url"
+    t.float    "distance"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
 end
